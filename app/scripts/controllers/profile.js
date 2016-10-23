7103b43b0c8844a599f86c9ace0c0f6ed7093b01
Mulhem <mulhem.hazam@gmail.com>
2016-10-23 16:21:29 +0400

    Schools

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -10,44 +10,100 @@ angular.module('sbAdminApp')
   .controller('ProfileCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
 $scope.options=[
 	{
-		name:"Manufacturing", 
+		name:"FS1", 
 		selected:false
 	},
 	{
-		name:"Retail", 
+		name:"FS2", 
 		selected:false	
 	},
 	{
-		name:"Construction", 
+		name:"Year 1", 
 		selected:false	
 	},
 
 	{
-		name:"Logistics", 
+		name:"Year 2", 
 		selected:false	
 	},
 	{
-		name:"Education", 
+		name:"Year 3", 
 		selected:false	
 	},
 	{
-		name:"Healthcare", 
+		name:"Year 4", 
 		selected:false	
 	},
 	{
-		name:"Oil and Gas", 
+		name:"Year 5", 
 		selected:false	
 	},
 	{
-		name:"Transportation", 
+		name:"Year 6", 
 		selected:false	
 	},
 	{
-		name:"Consultancy", 
+		name:"Year 7", 
 		selected:false	
 	},
 	{
-		name:"Hospitality", 
+		name:"Year 8", 
+		selected:false	
+	},
+	{
+		name:"Year 9", 
+		selected:false	
+	},
+	{
+		name:"Year 10", 
+		selected:false	
+	},
+	{
+		name:"Year 11", 
+		selected:false	
+	},
+	{
+		name:"Year 12", 
+		selected:false	
+	},
+	{
+		name:"Year 13", 
+		selected:false	
+	},
+	{
+		name:"Library", 
+		selected:false	
+	},
+	{
+		name:"Music Department", 
+		selected:false	
+	},
+	{
+		name:"ICT Department", 
+		selected:false	
+	},
+	{
+		name:"Gymnasium", 
+		selected:false	
+	},
+	{
+		name:"Art Room", 
+		selected:false	
+	},
+	{
+		name:"Swimming Pools", 
+		selected:false	
+	},
+	{
+		name:"Shaded Playing Areas", 
+		selected:false	
+	},
+	{
+		name:"Astro Turf Playing Areas", 
+		selected:false	
+	},
+	{
+		name:"Parent Areas and Meeting Rooms", 
 		selected:false	
 	}
 	];
