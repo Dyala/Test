43c923cdf961e372b2d3679f691ef1be9c7e87a0
Mulhem <mulhem.hazam@gmail.com>
2016-10-23 15:46:55 +0400

    initial

--- Gruntfile.js
+++ Gruntfile.js
@@ -68,8 +68,8 @@ module.exports = function (grunt) {
       options: {
 	  port: process.env.PORT || 8080,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
-        livereload: 35729
+        // hostname: 'localhost',
+        // livereload: 35729
       },
       livereload: {
         options: {
